d327d156a505c8b8990d70ce5dde672a644e762a
Crowdbotics <team@crowdbotics.com>
2020-08-01 10:02:46 +0000

    Update models

--- backend/fooddelivery_19302/wsgi.py
+++ backend/fooddelivery_19302/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fooddelivery_19302.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fooddelivery_19302.settings")
 
 application = get_wsgi_application()
